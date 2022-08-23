76a98714e699d0410ca6c531deaa9990178d3ff9
duycodedao <111243534+duycodedao@users.noreply.github.com>
2022-08-23 12:12:45 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// let btn = document.getElementById("mybtn");
+// btn.addEventListener("click", anhYeuEm = () => {
+//     for (let i = 0; i < 1; i++) {
+//         document.getElementById("bnc").innerHTML += "<br/>Bất Ngờ Chưa<br/>"
+//     }
+// })
+function copyText(){
+    navigator.clipboard.writeText("https://drive.google.com/file/d/1KbDkf-06KGxRX6CtKA8n5yrB2lT23ug9/view")
+}
